63fd85c9f9363447cecfcb9e2c437e470a745777
Harsh Vyapari <harshv1741@gmail.com>
2023-06-02 08:02:36 +0530

    Question 6: Done

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,42 @@ import StartButton from './StartButton';
 import Stationary from './Stationary';
 import Products from './Products';
 import Prodcuts2 from './Products2';
+import Student from './Student';
 
 function App() {
-    // For Question Number 1
-    const StyledButton = {
-        backgroundColor: 'lightgreen',
-        color: 'darkgreen',
-        borderRadius: '5px',
-        padding: '10px'
-    };
+  // For Question Number 1
+  const StyledButton = {
+    backgroundColor: 'lightgreen',
+    color: 'darkgreen',
+    borderRadius: '5px',
+    padding: '10px'
+  };
 
-    // For Question Number 2
-    const header = 'Stationary Items';
-    const items = ['pen', 'pencil', 'ruler', 'eraser'];
+  // For Question Number 2
+  const header = 'Stationary Items';
+  const items = ['pen', 'pencil', 'ruler', 'eraser'];
 
-    return (
-        <div>
-            {/* Question Number 1 */}
-            <Employee name={'Harsh'} designation={'Software Engineer'} education={'Under Graduate'} />
-            ------------------------
-            {/* Question Number 2 */}
-            <StartButton StyledButton={StyledButton} />
-            ------------------------
-            {/* Question Number 3 */}
-            <Stationary header={header} items={items} />
-            ------------------------
-            {/* Question Number 4 */}
-            <Products/>
-            ------------------------
-            {/* Question Number 5 */}
-            <Prodcuts2/>
-        </div>
-    );
+  return (
+    <div>
+      {/* Question Number 1 */}
+      <Employee name={'Harsh'} designation={'Software Engineer'} education={'Under Graduate'} />
+      ------------------------
+      {/* Question Number 2 */}
+      <StartButton StyledButton={StyledButton} />
+      ------------------------
+      {/* Question Number 3 */}
+      <Stationary header={header} items={items} />
+      ------------------------
+      {/* Question Number 4 */}
+      <Products />
+      ------------------------
+      {/* Question Number 5 */}
+      <Prodcuts2 />
+      ------------------------
+      {/* Question Number 6 */}
+      <Student/>
+    </div>
+  );
 }
 
 export default App;
--- src/Student.jsx
+++ src/Student.jsx
@@ -1,10 +1,40 @@
-
 function Student() {
-    return(
-        <div>
-            <h3>Student Data</h3>
-        </div>
-    )
+  const student = {
+    name: 'John Doe',
+    english: 90,
+    maths: 80,
+    computers: 70
+  };
+
+  const gradesAssignment = (total) => {
+    if (total >= 225) {
+      return 'A';
+    } else if (total >= 180) {
+      return 'B';
+    } else if (total >= 150) {
+      return 'C';
+    } else {
+      return 'D';
+    }
+  };
+
+  const total = student.english + student.maths + student.computers;
+
+  return (
+    <div>
+      <h3>Student Data</h3>
+      <h3>
+        <ul style={{ listStyle: 'none' }}>
+          <li>Name: {student.name}</li>
+          <li>English: {student.english}</li>
+          <li>Maths: {student.maths}</li>
+          <li>Computers: {student.computers}</li>
+          <li>Total Marks: {total}</li>
+          <li>Grades: {gradesAssignment(total)}</li>
+        </ul>
+      </h3>
+    </div>
+  );
 }
 
-export default Student
+export default Student;
